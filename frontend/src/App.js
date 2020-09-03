1c9c820338b054743ce71bb798d517648f27d5d9
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-09-03 18:08:14 -0400

    [components] add about and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import About from './components/About'
+import Footer from './components/Footer'
+import Welcome from './components/Welcome'
+
 
 function App() {
   return (
     <div className="App">
-      <h1>MERN AUTH frontend</h1>
+      <Welcome />
+      <Footer />
     </div>
   );
 }
