2899baf3440ff2b15af483e9bee2c08deaba61e8
Michaudsr <68039781+Michaudsr@users.noreply.github.com>
2020-09-04 12:48:36 -0400

    [components] fixed bug on Login.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
-import Axios from 'axios';
-const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Signup = () => {
     let [name, setName] = useState('');
-    let [email, setEmail] = useState('');
+    let [email, setEmail] = useState('')
     let [password, setPassword] = useState('');
     let [confirmPassword, setConfirmPassword] = useState('');
     let [redirect, setRedirect] = useState(false);
 
-    let handleName = (e) => {
+    const handleName = (e) => {
         setName(e.target.value);
     }
+
     const handleEmail = (e) => {
         setEmail(e.target.value);
     }
@@ -48,22 +48,22 @@ const Signup = () => {
             <div className="col-md-7 offset-md-3">
                 <div className="card card-body">
                     <h2 className="py-2">Signup</h2>
-                    <form action="/ideas" method="POST" onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit}>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
-                            <input type="text" name="name" value={name} onChange={handleName} className="form-control"></input>
+                            <input type="text" name="name" value={name} onChange={handleName} className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control"></input>
+                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control"></input>
+                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control"/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="confirmPassword">Confirm Password</label>
-                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleconfirmPassword} className="form-control"></input>
+                            <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="form-control"/>
                         </div>
                         <button type="submit" className="btn btn-primary float-right">Submit</button>
                     </form>
@@ -71,6 +71,7 @@ const Signup = () => {
             </div>
         </div>
     );
+
 }
 
 export default Signup;
